d21bf0ad901b7501c4991401c13ec8b865d8c960
Pouya <pooya.bazzaz@geeks.ltd.uk>
2019-05-09 17:23:30 +0100

    Organising

--- Core/Extensions/Extensions.cs
+++ Core/Extensions/Extensions.cs
@@ -11,14 +11,21 @@ namespace PyaFramework.Core
 {
     public static partial class Extensions
     {
+        public static string TrimEnd(this string @this, string tail)
+            => @this.EndsWith(tail) ? @this.Remove(@this.Length - tail.Length) : @this;
+
         public static string ToString<T>(this IEnumerable<T> source, string separator)
             => string.Join(separator, source);
 
         public static bool Lacks<T>(this IEnumerable<T> @this, T element)
             => @this.Contains(element) == false;
 
-        public static string TrimEnd(this string @this, string tail)
-            => @this.EndsWith(tail) ? @this.Remove(@this.Length - tail.Length) : @this;
+        public static IEnumerable<T> AppliedWithFilters<T>(this IEnumerable<T> source, IEnumerable<Func<T, bool>> filters)
+        {
+            foreach (var filter in filters)
+                source = source.Where(i => filter(i));
+            return source;
+        }
 
         public static IEnumerable<PropertyInfo> GetPublicInstancePropertyInfos(this Type T)
             => T.GetProperties(BindingFlags.Instance | BindingFlags.Public);
@@ -29,13 +36,6 @@ namespace PyaFramework.Core
         //Task: See if there's any built-in object binding facility in NetCore.
         public static string GetStringOfProperties<T>(this T obj)
             => typeof(T).GetPublicInstancePropertyInfos().Select(propInfo => propInfo.GetValue(obj)).ToString(" ");
-
-        public static IEnumerable<T> AppliedWithFilters<T>(this IEnumerable<T> source, IEnumerable<Func<T, bool>> filters)
-        {
-            foreach (var filter in filters)
-                source = source.Where(i => filter(i));
-            return source;
-        }
 
         //public static string[] GetNonRelationalColumnNames(this SqlDataReader @this)
         //{
