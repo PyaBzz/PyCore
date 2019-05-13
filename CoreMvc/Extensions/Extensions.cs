4fc4f446e1f51094367f1c8d70742f4b6b6aa1f6
Pouya <pooya.bazzaz@geeks.ltd.uk>
2019-05-13 18:06:35 +0100

    cleanup

--- CoreMvc/Extensions/Extensions.cs
+++ CoreMvc/Extensions/Extensions.cs
@@ -14,29 +14,5 @@ namespace PyaFramework.CoreMvc
     {
         public static string ShortNameOf<T>(this IHtmlHelper @this) where T : Controller
             => typeof(T).Name.TrimEnd("Controller");
-
-        public static IHtmlContent LinkToAction<ControllerType>(this IHtmlHelper @this, string methodName, string linkText = null, Guid id = default(Guid)) where ControllerType : Controller
-        {
-            var caption = linkText ?? methodName;
-            var controllerAreaAttribute = typeof(ControllerType).GetCustomAttribute(typeof(AreaAttribute));
-
-            dynamic routeValues = new ExpandoObject();
-
-            if (controllerAreaAttribute != null)
-            {
-                var areaName = typeof(AreaAttribute).GetProperty("RouteValue").GetValue(controllerAreaAttribute);
-                routeValues.area = areaName;
-            }
-
-            if (id != Guid.Empty)
-                routeValues.id = id;
-
-            return @this.ActionLink(
-                linkText: caption,
-                actionName: methodName,
-                controllerName: @this.ShortNameOf<ControllerType>(),
-                routeValues: routeValues as object
-                );
-        }
     }
 }
