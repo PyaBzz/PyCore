4865755aaf3237ab25748b5c90846ae6102b1c4c
Pouya <pooya.bazzaz@geeks.ltd.uk>
2019-05-31 12:06:43 +0100

    ShortName

--- CoreMvc/Extensions/Extensions.cs
+++ CoreMvc/Extensions/Extensions.cs
@@ -1,18 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using Microsoft.AspNetCore.Html;
-using PyaFramework.Core;
-using System.Dynamic;
-
-namespace PyaFramework.CoreMvc
+﻿namespace PyaFramework.CoreMvc
 {
     public static class Extensions
     {
-        public static string ShortNameOf<T>(this IHtmlHelper @this) where T : Controller
-            => typeof(T).Name.TrimEnd("Controller");
+        //public static string ShortNameOf<T>(this IHtmlHelper @this) where T : Controller
+        //    => typeof(T).Name.TrimEnd("Controller");
     }
 }
---
+++ CoreMvc/Services/ShortName.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PyaFramework.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PyaFramework.CoreMvc
+{
+    public static class ShortName
+    {
+        public static string Of<T>() where T : Controller
+            => typeof(T).Name.TrimEnd("Controller");
+    }
+}
