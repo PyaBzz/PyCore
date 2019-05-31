8184996078fde5991990d87919877193ea0a8604
Pouya <pooya.bazzaz@geeks.ltd.uk>
2019-05-31 16:13:08 +0100

    Access level names

--- Core/Constants.cs
+++ Core/Constants.cs
@@ -6,10 +6,10 @@
     {
         public const string SchemeName = "Cookies";
 
-        public const string Level0PolicyName = "Level0AndAbove";
-        public const string Level1PolicyName = "Level1AndAbove";
-        public const string Level2PolicyName = "Level2AndAbove";
-        public const string Level3PolicyName = "Level3AndAbove";
+        public const string Level0PolicyName = "Level0AndBelow";
+        public const string Level1PolicyName = "Level1AndBelow";
+        public const string Level2PolicyName = "Level2AndBelow";
+        public const string Level3PolicyName = "Level3AndBelow";
 
         public const string AdminRoleName = "Admin";
         public const string JuniorRoleName = "Junior";
