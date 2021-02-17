6a91409ba33401d5812752a7a4c0f25e405ad03c
pyabzz <>
2021-02-17 16:41:04 +0000

    Add tests

--- Test/Collections.cs
+++ Test/Collections.cs
@@ -7,16 +7,28 @@ namespace Test
     public class Collections
     {
         [Fact]
-        public void OrEmpty_Returns_Empty_if_Null()
+        public void OrEmpty_ReturnsEmpty_IfNull()
         {
             IEnumerable<int> unInitialised = null;
             Assert.Empty(unInitialised.OrEmpty());
         }
         [Fact]
-        public void OrEmpty_Returns_Itself_if_NotEmpty()
+        public void OrEmpty_ReturnsItself_IfNotEmpty()
         {
-            var myList = new List<int> { 1, 2, 3 };
-            Assert.Same(myList, myList.OrEmpty());
+            var list = new List<int> { 1, 2, 3 };
+            Assert.Same(list, list.OrEmpty());
+        }
+        [Fact]
+        public void ToString_GetsEmpty_IfEmpty()
+        {
+            var list = new List<int>();
+            Assert.Same(string.Empty, list.ToString("Separator"));
+        }
+        [Fact]
+        public void ToString_AddsNoSeparator_IfSingleElem()
+        {
+            var myList = new List<int> { 1 };
+            Assert.Same("1", myList.ToString("Separator"));
         }
     }
 }
